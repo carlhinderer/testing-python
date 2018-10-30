cb9195d5aa6a76936be2bb6f967c5ad503b03e5f
Carl Hinderer <carl.hinderer4@gmail.com>
2018-10-29 23:16:30 -0600

    Added tests for checking that task ids get assigned correctly.

--- tasks_proj/tests/func/test_add.py
+++ tasks_proj/tests/func/test_add.py
@@ -1,10 +1,26 @@
-"""
-Placeholder test file.
-
-We'll add a bunch of tests here in later versions.
-"""
+import pytest
+import tasks
+from tasks import Task
 
 
-def test_add():
-    """Placeholder test."""
-    pass
+def test_add_returns_valid_id():
+    # GIVEN an initialized tasks db
+    # WHEN a new task is added
+    # THEN returned task_id is of type int
+    new_task = Task('do something')
+    task_id = tasks.add(new_task)
+    assert isinstance(task_id, int)
+
+
+@pytest.mark.smoke
+def test_added_task_has_id_set():
+    # GIVEN an initialized tasks db
+    # AND a new task is added
+    new_task = Task('sit in chair', owner='me', done=True)
+    task_id = tasks.add(new_task)
+
+    # WHEN task is retrieved
+    task_from_db = tasks.get(task_id)
+
+    # THEN task_id matches id field
+    assert task_from_db.id == task_id
