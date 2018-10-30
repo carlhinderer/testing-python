5c9b6fe4a7661320d5db6ed95af0fe31f9bcfb29
Carl Hinderer <carl.hinderer4@gmail.com>
2018-10-29 23:59:22 -0600

    Finished notes for Chapter 2.

--- tasks_proj/tests/func/test_add.py
+++ tasks_proj/tests/func/test_add.py
@@ -24,3 +24,14 @@ def test_added_task_has_id_set():
 
     # THEN task_id matches id field
     assert task_from_db.id == task_id
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    # Setup: start db
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    yield
+
+    # Teardown: stop db
+    tasks.stop_tasks_db()
