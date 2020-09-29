a593505b9317c443dc6c9f5f5b09a3f34e5aaae2
Carl Hinderer <carl.hinderer4@gmail.com>
2020-09-29 12:04:03 -0600

    Added fixtures with test tasks.

--- tasks_proj/tests/conftest.py
+++ tasks_proj/tests/conftest.py
@@ -7,3 +7,25 @@ def tasks_db(tmpdir):
     tasks.start_tasks_db(str(tmpdir), 'tiny')
     yield
     tasks.stop_tasks_db()
+
+@pytest.fixture()
+def tasks_just_a_few():
+    return (
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False))
+
+@pytest.fixture()
+def tasks_mult_per_owner():
+    return (
+        Task('Make a cookie', 'Raphael'),
+        Task('Use an emoji', 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel'))
